aad70dc66b1a6cc9bf3c6e5924a192d01c303f8b
max <javoprav@gmail.com>
2023-06-28 20:32:57 +0300

    Initial commit and add models

---
+++ course/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='картинка', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+    name = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='lesson/', verbose_name='картинка', **NULLABLE)
+    url_video = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.name

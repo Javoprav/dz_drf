03fdb10aa38fc3d85b5bf06d1a302ebd25db2bc5
max <javoprav@gmail.com>
2023-07-12 20:51:21 +0300

    add IsAuthenticated

--- course/views.py
+++ course/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
+
 from course.models import Course, Lesson, Payments
 from course.serializers.serializers import CourseSerializers, LessonSerializers, PaymentsSerializers
 from rest_framework.filters import OrderingFilter
@@ -8,36 +10,43 @@ from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializers
     queryset = Course.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class CourseCreateAPIView(generics.CreateAPIView):
     serializer_class = CourseSerializers
     queryset = Course.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListView(generics.ListAPIView):
     serializer_class = LessonSerializers
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializers
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDetailView(generics.RetrieveAPIView):
     serializer_class = LessonSerializers
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateView(generics.UpdateAPIView):
     serializer_class = LessonSerializers
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDeleteView(generics.DestroyAPIView):
     serializer_class = LessonSerializers
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class PaymentsListView(generics.ListAPIView):
@@ -47,6 +56,7 @@ class PaymentsListView(generics.ListAPIView):
     filterset_fields = ['payment_type', 'course', 'lesson']
     filterset_class = FilterSet
     ordering_fields = ['payment_date']
+    permission_classes = [IsAuthenticated]
 
 """Фильтрация для эндпоинтов вывода списка платежей с возможностями:
 менять порядок сортировки по дате оплаты,
